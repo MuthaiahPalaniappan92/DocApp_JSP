be2823388b57935ac956802e9fe7a2363ca5fbca
c0652674 <c0652674@CPRO-W15-13.lambton.on.ca>
2015-07-11 11:17:53 -0400

    Email sending Application using EJB

---
+++ WebAppJAVA/JavaEmailApplication/src/java/emailSendingServlet.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import EJB.MailSender;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author c0652674
+ */
+@WebServlet(urlPatterns = {"/emailSendingServlet"})
+public class emailSendingServlet extends HttpServlet {
+    
+    
+    @EJB
+    private MailSender mail;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Mail Status</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Mail Status</h1>");
+            out.println("<b>Mail Sent successfully</b>");
+            out.println("Click<a href='index.html'>here</a> to go back");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        String toEmail=request.getParameter("toEmail");
+        String subject=request.getParameter("subject");
+        String message=request.getParameter("message");
+        String fromEmail="muthaiahpalaniappan92@gmail.com";
+        String userName="muthaiahpalaniappan92";
+        String password="Muthaiah92";
+        
+        mail.sendEmail(fromEmail, toEmail, subject, message, userName, password);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
